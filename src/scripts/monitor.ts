18f0a530d3b9acab3bdd8aafb425d041eeaae569
felixChB <lix.brunn@gmail.com>
2026-06-01 09:10:04 +0200

    removed error

--- src/scripts/monitor.ts
+++ src/scripts/monitor.ts
@@ -730,7 +730,7 @@ socket.on('forceReload', () => {
 // get all current Player Information from the Server at the start
 // and spawning all current players except yourself
 socket.on('currentState', (players: { [key: string]: Player }, ballColor: string,
-    playerStartInfosServer: { [key: number]: PlayerStartInfo }, sceneStartInfosServer: SceneStartInfos, autoJoin: boolean, gameTimerTime: number, serverInstanceId: string) => {
+    playerStartInfosServer: { [key: number]: PlayerStartInfo }, sceneStartInfosServer: SceneStartInfos, _autoJoin: boolean, gameTimerTime: number, serverInstanceId: string) => {
 
     const savedInstanceId = sessionStorage.getItem('serverInstanceId');
     if (savedInstanceId && savedInstanceId !== serverInstanceId) {
